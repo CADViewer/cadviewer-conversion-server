7c17fdb7e11d30e5feaa33f4ae1fab87e369ac57
CADViewer <61973235+CADViewer@users.noreply.github.com>
2024-06-04 20:13:06 +0200

    Docker update

--- content/inserted_image_objects/imageObjects.js
+++ content/inserted_image_objects/imageObjects.js
@@ -10,35 +10,7 @@ var cvjs_insertImageObjects = {
 						imageObject: "",
 						imageHeight: "450",
 						imageWidth: "600",
-						transform: "T846.4475927328996,1000.9180320541344S3.111666666666667"
-					}
-						,
-					OBJECT_2: {
-						name: "object2",
-						id: "2",
-						layer: "objectLayer",
-						group: "unassigned",
-						username: "Bob Smith",
-						userid: "user_1",
-						currentPage: 1,
-						imageObject: "",
-						imageHeight: "450",
-						imageWidth: "600",
-						transform: "T1267.5098101980466,1011.7841537951705S3.111666666666667"
-					}
-						,
-					OBJECT_3: {
-						name: "object3",
-						id: "3",
-						layer: "objectLayer",
-						group: "unassigned",
-						username: "Bob Smith",
-						userid: "user_1",
-						currentPage: 1,
-						imageObject: "",
-						imageHeight: "450",
-						imageWidth: "600",
-						transform: "T1158.848592787686,1161.193327734416S3.111666666666667"
+						transform: "T445.999008368168,176.54987510103908S2.3996833333333334"
 					}
 }
 
