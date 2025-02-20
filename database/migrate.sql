bbc9305f0b69f334c76e3146fb5eb5e5b8583caf
wamba zoko kévin <wambakevin7@gmail.com>
2025-02-20 08:59:10 +0000

    migrate.sql

---
+++ database/migrate.sql
@@ -0,0 +1,12 @@
+ALTER TABLE `users` ADD `folder_name` text NULL;
+ALTER TABLE `users`
+ADD `first_name` varchar(50) COLLATE 'latin1_swedish_ci' NULL,
+ADD `last_name` varchar(50) COLLATE 'latin1_swedish_ci' NULL AFTER `first_name`;
+ALTER TABLE `users`
+ADD `avatar_url` text COLLATE 'latin1_swedish_ci' NULL;
+
+
+ALTER TABLE `users`
+ADD `role` varchar(10) COLLATE 'latin1_swedish_ci' NOT NULL DEFAULT 'user';
+ALTER TABLE `users`
+ADD `is_enabled` tinyint NOT NULL DEFAULT '1';
