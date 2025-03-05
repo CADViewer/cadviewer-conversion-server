e7ef5ef972118798692ec8ad2040f08674229a1c
wamba zoko kévin <wambakevin7@gmail.com>
2025-03-05 10:54:19 +0000

    Merge branch 'master' of https://github.com/CADViewer/cadviewer-conversion-server-nextgen

---
+++ content/inserted_image_objects/content/drawings/dwg/one_of_simple.dwg/imageObjects.js
@@ -0,0 +1,35 @@
+var cvjs_insertImageObjects = {
+					"OBJECT_1": {
+						"name": "object1",
+						"id": "1",
+						"layer": "objectLayer",
+						"group": "unassigned",
+						"username": "User_cfn11k",
+						"userid": "user_id_cfn11k",
+						"currentPage": 1,
+						"imageObject": "40-Mango-Smoothie.28714219_std.png",
+						"imageHeight": "450",
+						"imageWidth": "500",
+						"transform": "T963.8750893644689,238.73701237170616S2.1479166666666667"
+					}
+						,
+					"OBJECT_2": {
+						"name": "object2",
+						"id": "2",
+						"layer": "objectLayer",
+						"group": "unassigned",
+						"username": "User_cfn11k",
+						"userid": "user_id_cfn11k",
+						"currentPage": 1,
+						"imageObject": "40-Mango-Smoothie.28714219_std.png",
+						"imageHeight": "450",
+						"imageWidth": "500",
+						"transform": "T1359.8711429029306,244.04053094588198S2.1479166666666667"
+					}
+}
+
+
+jQuery(document).ready(function() { 
+	imageObjects_loaded = true; 
+}); 
+imageObjects_loaded = true; 
