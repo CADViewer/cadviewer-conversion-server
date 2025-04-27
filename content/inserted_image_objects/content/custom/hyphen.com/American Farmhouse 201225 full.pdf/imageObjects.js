b841fbdbf2b7ad694315a988ac6c1d082b2feb68
wamba zoko kévin <wambakevin7@gmail.com>
2025-04-27 18:25:14 +0000

    AX v.15d update, L03-11J-lease.dwg with updated svg, json files

---
+++ content/inserted_image_objects/content/custom/hyphen.com/American Farmhouse 201225 full.pdf/imageObjects.js
@@ -0,0 +1,21 @@
+var cvjs_insertImageObjects = {
+					"OBJECT_1": {
+						"name": "object1",
+						"id": "1",
+						"layer": "objectLayer",
+						"group": "unassigned",
+						"username": "User_1omzi",
+						"userid": "user_id_1omzi",
+						"currentPage": 1,
+						"imageObject": "",
+						"imageHeight": "450",
+						"imageWidth": "600",
+						"transform": "T754.2551111920858,587.9698714754393S2.695"
+					}
+}
+
+
+jQuery(document).ready(function() { 
+	imageObjects_loaded = true; 
+}); 
+imageObjects_loaded = true; 
